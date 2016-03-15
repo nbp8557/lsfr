3ec08641afaaee519702e575d66902dfdb2897ca
nbp8557 <nbp8557@rit.edu>
2016-03-14 23:59:21 -0400

    Created a method for generating the result of the lsfr after a variable amount of steps. Also added a readme.

---
+++ README.md
@@ -0,0 +1,5 @@
+Requires Python 3.x
+
+You can run the application using the following command:
+
+    python application.py
--- application.py
+++ application.py
@@ -1,15 +1,19 @@
 
 
 '''
-    Calculates the result of the polynomial
-    (x^12 + x^11 + x^10 + x^4 + 1)
+    This method returns the binary result for the
+    polynomial:  (x^12 + x^11 + x^10 + x^4 + 1)
+
+    param:
+        bitArray - representation of the
+                    bit entire register
 '''
-def polynomial(bitarray):
+def polynomial(bitArray):
 
     #bit mask for bit 12
     mask = int('100000000000',2)
 
-    if mask & bitarray == 0:
+    if mask & bitArray == 0:
         bit12 = 0
     else:
         bit12 = 1
@@ -17,8 +21,8 @@ def polynomial(bitarray):
     #bit mask for bit 11
     mask = int('010000000000',2)
 
-    #bit12= mask & bitarray
-    if mask & bitarray == 0:
+    #bit12= mask & bitArray
+    if mask & bitArray == 0:
         bit11 = 0
     else:
         bit11 = 1
@@ -26,7 +30,7 @@ def polynomial(bitarray):
     #bit mask for bit 10
     mask = int('001000000000',2)
 
-    if mask & bitarray == 0:
+    if mask & bitArray == 0:
         bit10 = 0
     else:
         bit10 = 1
@@ -34,7 +38,7 @@ def polynomial(bitarray):
     #bit mask for bit 4
     mask = int('000000001000',2)
 
-    if mask & bitarray == 0:
+    if mask & bitArray == 0:
         bit4 = 0
     else:
         bit4 = 1
@@ -54,34 +58,62 @@ def polynomial(bitarray):
 '''
     This method returns the binary result of a single step
     for the polynomial:  (x^12 + x^11 + x^10 + x^4 + 1)
+
+    param:
+        bitArray - representation of the
+                    bit entire register
 '''
-def step(bitarray):
+def step(bitArray):
 
 
     # get the polynomial result
-    polynomialResult = polynomial(bitarray)
+    polynomialResult = polynomial(bitArray)
 
 
     #this is too prevent overflow of 12 bits
     mask = (2 ** 12 - 1)
 
     #now shift all the bits the left by 1 bit
-    bitarray = (bitarray << 1) & mask
+    bitArray = (bitArray << 1) & mask
 
     #now place the polynomialResult the first bit position
-    bitarray = bitarray | polynomialResult
+    bitArray = bitArray | polynomialResult
 
-    return bitarray
+    return bitArray
+
+'''
+    This method simulates x steps and returns the
+    bit representation
 
 
+    param:
+        bitArray - representation of the
+                    bit entire register
+        numSteps - number of steps to simulate
 
+
+'''
+def generate(bitArray,numSteps):
+
+    for x in range(numSteps):
+        bitArray = step(bitArray)
+
+    return bitArray
+
+'''
+    Just some tests for your review
+'''
 def test():
+    print("Tests are beginning")
+
     bitTest1 = int('011111111111',2)
 
     bitTest2 = int('111111111110',2)
+
+    print(bin(generate(bitTest1,2)))
     print(bin(step(bitTest1)))
     print(bin(step(bitTest2)))
-    print("tests")
+
 
 
 
